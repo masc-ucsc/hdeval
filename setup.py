aad8d0da192d7a6e1b4c4f422e6263554e1e1c94
Farzaneh Rabiey <farzane.rabiey@gmail.com>
2024-07-10 08:42:54 -0700

    Integrating with hdlagent

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hdeval',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Hardware Description Evaluation Interface',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/masc-ucsc/hdeval'
+)
+
