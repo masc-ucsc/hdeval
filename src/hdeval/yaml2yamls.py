84bcf319e598785182254e89157016f62e14a5de
Farzaneh Rabiey <farzane.rabiey@gmail.com>
2024-10-09 09:12:59 -0700

    Script has chnaged to add golden design section in yaml files

--- src/hdeval/yaml2yamls.py
+++ src/hdeval/yaml2yamls.py
@@ -17,16 +17,32 @@ def main():
     with open(input_yaml, 'r') as file:
         data = yaml.safe_load(file)
         print("YAML file loaded successfully.")
+    #Added for the responce section
+    class FoldedDumper(yaml.SafeDumper):
+        """
+        A YAML dumper that forces folded style for multi-line strings.
+        """
 
-    # Example processing: Create separate YAML files for each problem
+    def folded_representer(dumper, data):
+        """
+        This representer will convert multiline strings to folded style.
+        """
+        if '\n' in data:
+            return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='>')
+        else:
+            return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+    FoldedDumper.add_representer(str, folded_representer)
+    #end here
     for problem in data.get('verilog_problems', []):
         bench_name = problem.get('bench_name', 'unknown')
         output_filename = os.path.join(output_dir, f"{bench_name}_spec.yaml")
         with open(output_filename, 'w') as outfile:
             yaml.dump({
                 'description': problem.get('description', ''),
-                'interface': problem.get('interface', '')
-            }, outfile, default_flow_style=False, sort_keys=False)
+                'interface': problem.get('interface', ''),
+                'bench_response': problem.get('bench_response', '')
+            }, outfile, Dumper=FoldedDumper, default_flow_style=False, sort_keys=False)
         print(f"File written: {output_filename}")
 
     print("All files have been successfully created.")
